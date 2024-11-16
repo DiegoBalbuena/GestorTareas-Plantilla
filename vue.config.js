b53c300df8b2aed5da96710e9bde8fe56882dc13
DiegoBalbuena <69486706+DiegoBalbuena@users.noreply.github.com>
2024-11-15 22:18:52 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/GestorTareas-Plantilla/'
-    : '/'
+    : '/',
+  outputDir: 'dist',  // Explicitly define output directory
 });
