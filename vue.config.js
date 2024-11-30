2779a81fe6af5b9caa14ac466b6455c5b0cdc77a
DiegoBalbuena <69486706+DiegoBalbuena@users.noreply.github.com>
2024-11-30 10:36:22 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/todo-project/'  // Reemplaza con el nombre exacto de tu repositorio
+    ? '/GestorTareas-Plantilla/'  // Reemplaza con el nombre exacto de tu repositorio
     : '/'
 });
